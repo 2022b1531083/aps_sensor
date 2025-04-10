93fb4cb3833553fdbc7d0a8d0e5cf1edfe4ef3e1
Yogesh Kumar <yogiafse@gmail.com>
2025-04-10 15:01:11 +0530

    add exception and logger file for detection errors in efficient way

---
+++ main.py
@@ -0,0 +1,23 @@
+from Sensor.exception import SensorException
+import os 
+import sys
+
+from Sensor.logger import logging
+
+def test_exception():
+    try:
+        logging.info("ki yaha p bhaiaa ek error ayegi diveision by zero wali error ")
+        a=1/0
+    except Exception as e:
+       raise SensorException(e,sys) 
+      
+
+
+
+
+if __name__ == "__main__":
+
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
